2ca0ce007adfc51c071ba7033900d3861b59f473
SamCPerlmutter <samuel.perlmutter21@gmail.com>
2024-12-11 00:32:15 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Success")
+
+const button = document.getElementById("John");
+
+button.addEventListener("click", function(){
+    const navbar = document.getElementById("navbar");
+    navbar.classList.remove("navcolormain")
+    navbar.classList.add("navcolor2")
+
+    })
