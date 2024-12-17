2d5d5fe2f445caae7ef4f65d9517e96d4e05e7e3
SamCPerlmutter <samuel.perlmutter21@gmail.com>
2024-12-17 00:07:06 +0000

    Calculator added with js

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ alert("Success")
 
 const button = document.getElementById("John");
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function(e){
+    e.target.value
     const navbar = document.getElementById("navbar");
     navbar.classList.remove("navcolormain")
     navbar.classList.add("navcolor2")
 
     })
+
+//Find out which button was clicked
+//Need to use an array
+//For loop
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
